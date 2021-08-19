4c220b91cfafc63d1810b91d9a70ccb7e8edcc6f
0xsapphir3 <0xsapphir3@gmail.com>
2021-08-19 13:11:05 +0530

    Fix broken urls

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup , find_packages
 
 setup(
-    name='GalleryMan',
+    name='galleryman',
     version='1.0.0',
     description='A Tool To Manage Your Memories!',
     author='0xsapphir3',
     author_email='0xsapphir3@gmail.com',
-    url='https://github.com/0xsapphir3/Galleryman',
-    download_url='https://github.com/0xsapphir3/Galleryman/tarball/1.0.0',
+    url='https://github.com/0xsapphir3/galleryman',
     entry_points={
         'console_scripts': [
             'galleryman = GalleryMan.main:main',
         ],
     },
-    install_requires=['pyqt5' , 'pillow' , 'numpy' , 'functools' , 'configparser' , 'json' , 'pathlib' , 'math'],
+    install_requires=['pyqt5' , 'pillow' , 'numpy' , 'functools' , 'configparser' , 'pathlib'],
     packages=find_packages()
 )
